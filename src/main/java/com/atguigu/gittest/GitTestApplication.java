b5a7afa076c16cbc5062f85a5863aab38808a9a6
bin <bin@2930011258.com>
2024-01-13 14:32:22 +0800

    push commit

--- src/main/java/com/atguigu/gittest/GitTestApplication.java
+++ src/main/java/com/atguigu/gittest/GitTestApplication.java
@@ -14,6 +14,7 @@ public class GitTestApplication {
         System.out.println("hello git4");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 
 }
