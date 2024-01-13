8c449ea0caf7dfb166845aa6af6092402b62c1bb
yuanbinyyds <156431584+yuanbinyyds@users.noreply.github.com>
2024-01-13 14:48:22 +0800

    pull commit

--- src/main/java/com/atguigu/gittest/GitTestApplication.java
+++ src/main/java/com/atguigu/gittest/GitTestApplication.java
@@ -15,6 +15,7 @@ public class GitTestApplication {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 
 }
