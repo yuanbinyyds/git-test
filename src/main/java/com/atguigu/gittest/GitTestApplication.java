7e779c4993de3132ef6aba0825f97a53c44d93a3
bin <bin@2930011258.com>
2024-01-13 13:47:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/gittest/GitTestApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.gittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+//        SpringApplication.run(GitTestApplication.class, args);
+    }
+
+}
